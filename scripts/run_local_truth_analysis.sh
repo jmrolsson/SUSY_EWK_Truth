ba256d56be9a89b75bce3d409993f431c2d52441
Joakim Olsson <j.mr.olsson@gmail.com>
2017-02-07 10:31:15 -0600

    Updated file lists

--- scripts/run_local_truth_analysis.sh
+++ scripts/run_local_truth_analysis.sh
@@ -7,6 +7,6 @@ mkdir -p results
 # for sample in WZ_500p0_200p0 WZ_500p0_0p5 WZ_300p0_0p5 WZ_200p0_0p5 WZ_150p0_0p5 Wh_500p0_200p0 Wh_500p0_0p5 Wh_300p0_0p5 Wh_200p0_0p5 Wh_150p0_0p5;
 for sample in Wh_500p0_200p0;
 do
-  xAH_run.py --files SUSY_EWK_Truth/filelists/mc15_13TeV_C1N2_${sample}_bbqq_J10.txt --inputList --config SUSY_EWK_Truth/scripts/truth_analysis_config.py --submitDir results/C1N2_${sample}_bbqq_J10 --verbose --force direct
+  echo xAH_run.py --files SUSY_EWK_Truth/filelists/mc15_13TeV_C1N2_${sample}_bbqq_J10.txt --inputList --config SUSY_EWK_Truth/scripts/truth_analysis_config.py --submitDir results/C1N2_${sample}_bbqq_J10 --verbose --force direct
 done;
 
